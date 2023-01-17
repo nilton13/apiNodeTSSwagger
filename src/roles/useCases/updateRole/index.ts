cf24e78d1411f915be91a9e41b8570943328b050
Nilton F <jnvlfilho@hotmail.com>
2023-01-17 13:30:46 -0300

    Criando useCase para alterar uma role

---
+++ src/roles/useCases/updateRole/index.ts
@@ -0,0 +1,9 @@
+import { RolesRepository } from '@roles/repositories/RolesRepository';
+import { UpdateRoleController } from './UpdateRoleController';
+import { UpdateRoleUseCase } from './UpdateRoleUseCase';
+
+const rolesRepository = RolesRepository.getInstance();
+const updateRoleUseCase = new UpdateRoleUseCase(rolesRepository);
+export const updateRolesController = new UpdateRoleController(
+  updateRoleUseCase,
+);

d87f4f53765ee83f71199812d88833545847d346
Nilton F <jnvlfilho@hotmail.com>
2023-01-17 12:43:13 -0300

    Instalação e configuração do TypeORM

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
+import { Role } from '@roles/entities/Role';
 import { DataSource } from 'typeorm';
 import { CreateRolesTable1673465678142 } from './migrations/1673465678142-CreateRolesTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1673465678142],
 });

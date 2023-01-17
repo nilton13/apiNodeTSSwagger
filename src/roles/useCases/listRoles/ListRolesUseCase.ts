c96de273fac7cc4de853c03946d514139ea01926
Nilton F <jnvlfilho@hotmail.com>
2023-01-17 13:04:26 -0300

    Refatorando usecases e adicionando paginação a lista de roles

--- src/roles/useCases/listRoles/ListRolesUseCase.ts
+++ src/roles/useCases/listRoles/ListRolesUseCase.ts
@@ -1,10 +1,23 @@
 import { Role } from '@roles/entities/Role';
-import { RolesRepository } from '@roles/repositories/RolesRepository';
+import {
+  RolesPaginateProps,
+  RolesRepository,
+} from '@roles/repositories/RolesRepository';
+
+type ListRolesUseCaseParams = {
+  page: number;
+  limit: number;
+};
 
 export class ListRolesUseCase {
   constructor(private rolesRepository: RolesRepository) {}
 
-  execute(): Role[] {
-    return this.rolesRepository.findAll();
+  async execute({
+    page,
+    limit,
+  }: ListRolesUseCaseParams): Promise<RolesPaginateProps> {
+    const take = limit;
+    const skip = (Number(page) - 1) * take;
+    return await this.rolesRepository.findAll({ page, skip, take });
   }
 }

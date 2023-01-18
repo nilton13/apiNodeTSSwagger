8dfa2ea376296a704e38f5d2d6542fd584b445f8
Nilton F <jnvlfilho@hotmail.com>
2023-01-17 21:40:15 -0300

    Create migration CreateUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { Role } from '@roles/entities/Role';
 import { DataSource } from 'typeorm';
 import { CreateRolesTable1673465678142 } from './migrations/1673465678142-CreateRolesTable';
+import { CreateUsersTable1674002017314 } from './migrations/1674002017314-CreateUsersTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1673465678142],
+  migrations: [CreateRolesTable1673465678142, CreateUsersTable1674002017314],
 });
---
+++ src/shared/typeorm/migrations/1674002017314-CreateUsersTable.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsersTable1674002017314 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'string',
+          },
+          {
+            name: 'email',
+            type: 'string',
+            isUnique: true,
+          },
+          {
+            name: 'avatar',
+            type: 'string',
+            isNullable: true,
+          },
+          {
+            name: 'isAdmin',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'password',
+            type: 'string',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}

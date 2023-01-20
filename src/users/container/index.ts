10ba5bce3406b2f946d53b630879379d137d4855
Nilton F <jnvlfilho@hotmail.com>
2023-01-20 09:28:17 -0300

    Criando rota de criação de usuários

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,5 +1,7 @@
 import { IUserRepository } from '@users/repositories/IUserRepository';
 import { UserRepository } from '@users/repositories/UserRepository';
+import { CreateUserController } from '@users/useCases/createUser/CreateUserController';
 import { container } from 'tsyringe';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton('CreateUserController', CreateUserController);
